badc0cd85a005dcb734ab628371ba36febee7b37
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2021-12-25 19:18:29 +0530

    added all crud funcitonaliyt to function based api views

--- restapp/urls.py
+++ restapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from restapp import views
 urlpatterns = [
     path('',views.home),
-    path('student/',views.studentjson)
+    path('student/',views.studentjson),
+    path('student/<str:pk>',views.students_details)
+
 ]
--- restapp/views.py
+++ restapp/views.py
@@ -14,9 +14,42 @@ def home(request):
     }
     return Response(apiurls)
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentjson(request):
-    studentobj=student.objects.all()
-    serialobj=studentserializer(studentobj,many=True)
-    return Response(serialobj.data)
+    if request.method == 'GET':
+        studentobj = student.objects.all()
+        serialobj = studentserializer(studentobj, many=True)
+        return Response(serialobj.data)
+    elif request.method == 'POST':
+        serializer = studentserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','PUT','DELETE'])
+def students_details(request,pk):
+    try:
+        studentobj = student.objects.get(pk=pk)
+    except Students.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializers = studentserializer(studentobj)
+        return Response(serializers.data)
+
+    elif request.method == 'PUT':
+        serializers = studentserializer(studentobj,request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        studentobj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
 
